bea33009fd9e43bd4702cf0f29ac28ce3c5ad1b5
gitds410 <111192681+gitds410@users.noreply.github.com>
2022-09-12 23:57:45 +0530

    Add files via upload

---
+++ PRACTICE QUESTIONS ON VARIABLE DATA TYPES/bike.java
@@ -0,0 +1,13 @@
+class bike
+{
+	public static void main(String[] args) 
+	{
+		
+		int bike_cost = 85000;
+		String bike_brand = "YAMAHA";
+		String bike_colour = "Black";
+		System.out.println("The Bike colour is "+ bike_colour);
+		System.out.println("The Bike Brand is "+ bike_brand);
+		System.out.println("The Bike cost is "+ bike_cost);
+	}
+}
