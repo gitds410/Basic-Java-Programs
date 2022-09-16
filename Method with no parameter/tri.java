7a16296bfec46de651bdbbf56cd06c57043f407e
gitds410 <111192681+gitds410@users.noreply.github.com>
2022-09-16 22:31:13 +0530

    Add files via upload

---
+++ Method with no parameter/tri.java
@@ -0,0 +1,21 @@
+class tri
+{
+	static void Area()
+	{ 
+		double a= 0.5;
+		int b=5;
+		int c= 4;
+	
+		double d = a*b*c;
+		System.out.println(d);
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("***Main Starts***");
+
+			Area();
+
+		System.out.println("***Main Ends***");
+	}
+}
+
