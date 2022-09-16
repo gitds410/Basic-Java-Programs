082ec88592dfdf05f04d12a855b50ad32777df06
gitds410 <111192681+gitds410@users.noreply.github.com>
2022-09-16 22:04:13 +0530

    Create rectangle.java

---
+++ Method with Parameter/rectangle.java
@@ -0,0 +1,17 @@
+class rect
+{
+	static double area()
+	{ 
+		int a= 8;
+		int b = 5;
+		int c = a*b;
+		return c;
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("***Main Starts***");
+		double x = area();
+		System.out.println(x);
+		System.out.println("***Main Ends***");
+	}
+}
