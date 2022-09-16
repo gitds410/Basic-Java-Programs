61cc095a1121006ab81f7a28566310e15bc474f4
gitds410 <111192681+gitds410@users.noreply.github.com>
2022-09-16 22:14:16 +0530

    Create circle.java

---
+++ METHOD WITH PARAMETER/circle.java
@@ -0,0 +1,16 @@
+class  CIRCLEI
+{ 
+	static void area(int r)
+		{
+			final double p = 3.14;
+			double c = p*r*r;
+			System.out.println(c);
+		}
+
+public static void main (String [] args)
+	{ 
+		area(5);
+	}
+	
+}
+
