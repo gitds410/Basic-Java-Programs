5820821e474e8e10c2b69c16e6563f3bc6e5cba5
gitds410 <111192681+gitds410@users.noreply.github.com>
2022-09-16 22:26:33 +0530

    Add files via upload

---
+++ Method with Parameter/par.java
@@ -0,0 +1,20 @@
+class par
+{
+	static void Area()
+	{ 
+		
+		int b=5;
+		int c= 4;
+	
+		double d = b*c;
+		System.out.println(d);
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("***Main Starts***");
+
+			Area();
+
+		System.out.println("***Main Ends***");
+	}
+}
---
+++ Method with Parameter/sec.java
@@ -0,0 +1,21 @@
+class sec
+{
+	static void Area()
+	{ 
+		double s= 0.5;
+		int h=5;
+		int c= 4;
+		int a= 2;
+	
+		double d = s*(a+c)*h;
+		System.out.println(d);
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("***Main Starts***");
+
+			Area();
+
+		System.out.println("***Main Ends***");
+	}
+}
