7285127ba13468bccb52671e9c90b7c183b35377
gitds410 <111192681+gitds410@users.noreply.github.com>
2022-09-16 22:20:53 +0530

    Add files via upload

---
+++ METHOD WITH PARAMETER/square.java
@@ -0,0 +1,19 @@
+class square
+{
+	static void Area( int a)
+	{ 
+		
+		
+		double c = a*a;
+		System.out.println(c);
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("***Main Starts***");
+
+			Area(5);
+
+		System.out.println("***Main Ends***");
+	}
+}
+
